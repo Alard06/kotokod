eed2ac8f3dddb68fdcee8a7284cd5b0cf7ad2970
Nikita <NikitaMineev2020@yandex.ru>
2024-11-10 19:27:18 +0800

    opencv cont2

--- PRO/17 openCV.py
+++ PRO/17 openCV.py
@@ -1,15 +1,24 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('img_1.png' )
+img = cv2.imread('img.png' )
+
+# h = img.shape[0]
+# w = img.shape[1]
+#
+# for i in range(h):
+#    for a in range(w):
+#        for b in range(3):
+#            img[i][a][b] += np.uint8(127)
+
 
 h = img.shape[0]
 w = img.shape[1]
 
-for i in range(h):
-   for a in range(w):
-       for b in range(3):
-           img[i][a][b] += np.uint8(10)
+z = np.zeros((h,w,3) , dtype='uint8' )
+z[...] = np.uint8(100) #меняем 100 для регулировки
 
-cv2.imshow( 'zzz', img)
+result = cv2.add( img , z) #меняем add на sabtract чтобы #затемнять
+
+cv2.imshow( 'zzz', result)
 cv2.waitKey(0)
