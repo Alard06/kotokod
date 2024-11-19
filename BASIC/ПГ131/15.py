ab47bce0f83b98811f28c1b8d955bab61dca270d
Nikita <NikitaMineev2020@yandex.ru>
2024-11-19 22:12:37 +0800

    pygame basic 2

--- BASIC/ПГ131/15.py
+++ BASIC/ПГ131/15.py
@@ -1,8 +1,12 @@
 import pygame as pg
+import random
 
 pg.init()
 
-
+r = 0
+g = 0
+b = 0
+radius = 0
 width, height = 1000, 800
 screen = pg.display.set_mode((width, height))
 
@@ -14,9 +18,25 @@ while True:
         if event.type == 256:
             pg.quit()
             exit(0)
-        if event.type == 1025:
+        if event.type == 1024:
             x, y = event.pos
-            pg.draw.circle(screen, (255, 0, 0), (x, y), 10)
+            btns = event.buttons
+            if btns == (1, 0, 0):
+                print("Button")
+                pg.draw.circle(screen, (255, 255, 255), (x, y), radius)
+            # if event.button == 4:
+            #     radius += 1
+            # if event.button == 5:
+            #     radius -= 1
+        if event.type == 768:
+            id_key = event.key
+            if id_key == pg.K_1:
+                r = random.randint(0, 255)
+                g = random.randint(0, 255)
+                b = random.randint(0, 255)
+
+            if id_key == pg.K_2:
+                radius += 1
 
     pg.display.flip()
 
